92ef25cbae6f058d29edf7a9b18c31971a6d7f1f
chrisbooth <chris.booth.ct@gmail.com>
2024-02-26 21:56:06 +0200

    Completed Navbar functionality

--- my-portfolio/src/components/NavBar.js
+++ my-portfolio/src/components/NavBar.js
@@ -8,12 +8,23 @@ const NavBar = () => {
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
 
+    const [color, setColor] = useState(false);
+    const changeColor = () => {
+        if(window.scrollY >= 100){
+            setColor(true);
+        } else{
+            setColor(false);
+        }
+    }
+
+    window.addEventListener("scroll", changeColor)
+
     return (
-        <div className="header">
+        <div className={color ? "header header-bg" : "header"}>
             <Link to="/">
                 <h1>Portfolio</h1>
             </Link>
-            <ul className="nav-menu">
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
@@ -27,9 +38,11 @@ const NavBar = () => {
                     <Link to="/contact">Contact</Link>
                 </li>
             </ul>
-            <div className="hamburger">
-                <FaTimes size={20} style={{ color: "#fff" }} />
-                <FaBars size={20} style={{ color: "#fff" }} />
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (<FaTimes size={20} style={{ color: "#fff" }} />) : 
+                (<FaBars size={20} style={{ color: "#fff" }} />)}
+                
+                
             </div>
         </div>
     )
